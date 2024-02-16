24b4c8b20bc3be8a6ba119046fab9901cd77144d
Caleb Williams <br0p0p@users.noreply.github.com>
2024-02-16 10:24:13 -0600

    Bug repro

---
+++ cookiebug.mjs
@@ -0,0 +1,27 @@
+import { createCookie } from "@remix-run/node";
+
+const firstCookie = createCookie("firstCookie", {
+  httpOnly: true,
+});
+
+const secondCookie = createCookie("secondCookie", {
+  httpOnly: true,
+});
+
+const headers = new Headers();
+headers.append("Set-Cookie", await firstCookie.serialize({ value: 1 }));
+headers.append("Set-Cookie", await secondCookie.serialize({ value: 2 }));
+
+const response = new Response("{}", { headers });
+
+console.log("cookie val:", response.headers.get("Set-Cookie"));
+
+console.log(
+  "firstCookie:",
+  await firstCookie.parse(response.headers.get("Set-Cookie"))
+);
+
+console.log(
+  "secondCookie:",
+  await secondCookie.parse(response.headers.get("Set-Cookie"))
+);
